12feab64f11570c0c50c5b45f558402edd8fe33b
Breaking_Lead <s25nnkerman@foxmail.com>
2022-01-06 10:13:03 +0800

    add fast_pow.cpp and p2615

---
+++ list-4-array/p2615.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+int mat[45][45];
+int main()
+{
+    int n;
+    cin>>n;
+    n=n%2==1?n:n+1;
+    mat[1][((n+1)/2)] = 1;
+    //for (int i = 1; i <= n; i++)
+    //{
+    //    for (int j = 1; j <= n; j++)
+    //    {
+    //        cout << mat[i][j] << " ";
+    //    }
+    //    cout << endl;
+    //}
+    int row;
+    int column
+    for(int k=2;k<=n*n;k++){
+        row=k/n;
+        column=k-n*row;
+        cout<<row<<" "<<column<<endl;
+    }
+    return 0;
+}
