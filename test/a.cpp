9318426b2406ca1a9f63d249d61fc1263dc2ef38
Breaking_Lead <s25nnkerman@foxmail.com>
2022-01-08 15:12:18 +0800

    i don't know what i commit

---
+++ test/a.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <vector>
+using namespace std;
+enum STATE
+{
+    INVISIBLE = 1,
+    ONLINE_BUT_DONT_MIND_ANYONE,
+    ONLINE_AND_ACTIVE,
+    OFFLINE,
+    UNKNOWN
+};
+struct Wu
+{
+    STATE state;
+    Wu(STATE _state){
+        state=_state;
+    }
+};
+int main()
+{
+    Wu wu1();
+    if(wu1)
+    return 0;
+}
