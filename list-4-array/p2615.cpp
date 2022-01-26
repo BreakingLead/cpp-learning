7db5aa2e571c41ab80750b056cceb1ba27e814c9
Breaking_Lead <s25nnkerman@foxmail.com>
2022-01-26 19:30:25 +0800

    Acc a question

--- list-4-array/p2615.cpp
+++ list-4-array/p2615.cpp
@@ -2,27 +2,37 @@
 #include <cstdio>
 #include <algorithm>
 using namespace std;
-int mat[45][45];
+int mat[45][45], n, x, y;
 int main()
 {
-    int n;
-    cin>>n;
-    n=n%2==1?n:n+1;
-    mat[1][((n+1)/2)] = 1;
-    //for (int i = 1; i <= n; i++)
-    //{
-    //    for (int j = 1; j <= n; j++)
-    //    {
-    //        cout << mat[i][j] << " ";
-    //    }
-    //    cout << endl;
-    //}
-    int row;
-    int column;
-    for(int k=2;k<=n*n;k++){
-        row=k/n+1;
-        column=k%n+1;
-        cout<<row<<" "<<column<<endl;
+    scanf("%d", &n);
+    mat[1][(n / 2) + 1] = 1;
+    x = 1;
+    y = n / 2 + 1;
+    for (int i = 2; i <= n * n; i++)
+    {
+        if (x == 1 && y != n)
+            mat[n][y + 1] = i, x = n, y++;
+        else if (x != 1 && y == n)
+            mat[x - 1][1] = i, x--, y = 1;
+        else if (x == 1 && y == n)
+            mat[2][n] = i, x = 2;
+        else if (x != 1 && y != n)
+        {
+            if (mat[x - 1][y + 1] == 0)
+                mat[x - 1][y + 1] = i, x--, y++;
+            else
+                mat[x + 1][y] = i, x++;
+            continue;
+        }
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            printf("%d ", mat[i][j]);
+        }
+        printf("\n");
     }
     return 0;
 }
