68c9373d08fd1a9766bd208178980ced41b3800c
BreakingLead <s25nnkerman@foxmail.com>
2022-05-31 22:48:01 +0800

    P3392

--- luogu/algorithm-1-3-enum/P3392.cpp
+++ luogu/algorithm-1-3-enum/P3392.cpp
@@ -6,23 +6,23 @@
 #include <string>
 #define MAXN 1005
 using namespace std;
-char a[MAXN][MAXN];
-int wcost[MAXN];
-int bcost[MAXN];
-int rcost[MAXN];
-int main() {
-    #ifndef ONLINE_JUDGE
-    freopen("/home/guo/Devel/cpp-learning/luogu/std.in","r",stdin);
-    #endif // !ONLINE_JUDGE
-    int n,m;
+char a[ MAXN ][ MAXN ];
+int  wcost[ MAXN ];
+int  bcost[ MAXN ];
+int  rcost[ MAXN ];
+int  main() {
+
+#ifndef ONLINE_JUDGE
+    freopen( "/home/guo/Devel/cpp-learning/luogu/std.in", "r", stdin );
+#endif // !ONLINE_JUDGE
+       // 
+    int n, m, minval = 0x3f3f3f;
 
     for ( int i = 1; i <= n; i++ ) {
         for ( int j = 1; j <= m; j++ ) {
-            scanf("%c",&a[i][j]);
+            scanf( "%c", &a[ i ][ j ] );
         }
     }
-    for(int i=1;i<=n;i++){
-
-    }
+    printf( "%d\n", minval );
     return 0;
 }
